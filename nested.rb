582bcf442dd31d72eae24641d78adb6f13539954
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-30 14:23:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => Ruby,
+    :languages => ["LISP", "C"]}
+    #   :known_for => "Ruby",
+    #   :languages => ["LISP", "C"]
+    # }
 end
 
 def changing_alan
